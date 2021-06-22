1d89074f4d8b14d89eac71a2faa3de43d1be16ee
Giovanni Visciano <giovanni.visciano@athonet.com>
2021-06-22 17:46:22 +0200

    test

--- .github/cache_import.sh
+++ .github/cache_import.sh
@@ -2,11 +2,9 @@
 
 set -e
 
-sudo chown -R root:root .cache
-
-for CACHE_DIR in .cache/*; do
-    VOLUME=$(basename $CACHE_DIR)
+for VOLUME_TAR in .cache/*.tar; do
+    VOLUME=$(basename -s .tar $VOLUME_TAR)
     
     echo "Restoring docker volume $VOLUME"
-    docker run --rm -v $PWD/$CACHE_DIR:/source -v $VOLUME:/dest alpine cp -rT /source /dest
+    docker run --rm -v $PWD/$VOLUME_TAR:/source/$VOLUME_TAR -v $VOLUME:/volume alpine tar -xpf /source/$VOLUME_TAR
 done
