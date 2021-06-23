6c8466e2428c07612ea78852d95c91633789bde7
Giovanni Visciano <giovanni.visciano@athonet.com>
2021-06-23 18:29:34 +0200

    test

--- .github/cache_import.sh
+++ .github/cache_import.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-for VOLUME_TAR in .cache/*.tar; do
-    VOLUME=$(basename -s .tar $VOLUME_TAR)
-    
-    echo "Restoring docker volume $VOLUME"
-    docker run --rm -v $PWD/$VOLUME_TAR:/source/$VOLUME_TAR -v $VOLUME:/volume alpine tar -xpf /source/$VOLUME_TAR
-done
+if [ "$(ls -A .cache/*.tar)" ]; then
+    for VOLUME_TAR in .cache/*.tar; do
+        VOLUME=$(basename -s .tar $VOLUME_TAR)
+        
+        echo "Restoring docker volume $VOLUME"
+        docker run --rm -v $PWD/$VOLUME_TAR:/source/$VOLUME_TAR -v $VOLUME:/volume alpine tar -xpf /source/$VOLUME_TAR
+    done
+fi
