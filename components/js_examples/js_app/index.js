fb980228d0013e14aa625ca5d4f1d638cbe09548
Giovanni Visciano <giovanni.visciano@athonet.com>
2022-01-25 09:41:12 +0100

    pack ...

--- components/js_examples/js_app/index.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-import * as js_library from "js_library";
-import _ from "lodash";
-
-js_library.printHello();
-console.log(_.partition([1, 2, 3, 4], (n) => n % 2));
--- toolchains/build/js/toolchain.sh
+++ toolchains/build/js/toolchain.sh
@@ -15,18 +15,24 @@ case $1 in
         tar czf cache.tgz .pnpm-store
         mv cache.tgz .cache
         ;;
+    deps)
+        rm -rf .mbs-deps/*/package/
+        find .mbs-deps -name *.tgz -exec sh -c '
+            echo "extracting dependency {}"
+            tar xzf {} -C $(dirname {})
+        ' \;
+        ;;
     lint)
-        prettier --check . "!.{build,mbs-deps,pnpm-store}/**/*" "!.mbs-*.json" "!pnpm-lock.yaml"
+        prettier --check . "!.{build,mbs-deps,pnpm-store}/**/*" "!dist/**/*" "!.mbs-*.json" "!pnpm-lock.yaml"
         ;;
     build)
         CACHE_FROM=.mbs-deps/$MBS_ID-cache/cache.tgz
         if [ -f $CACHE_FROM ]; then
             echo "Using cached deps"
             tar xzf $CACHE_FROM
-        else
-            pnpm install --frozen-lockfile
         fi
 
+        pnpm install --frozen-lockfile
         pnpm build
         pnpm pack --pack-destination=.build/
         ;;
